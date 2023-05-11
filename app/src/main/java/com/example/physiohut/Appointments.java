a04aff7c7cb08e2d3743426d972e2cddea2ad9c1
Miltiadis papadopoulos <iis21063@uom.edu.gr>
2023-05-11 14:07:55 +0300

    R7 changes
    intit changes

--- app/src/main/java/com/example/physiohut/Appointments.java
+++ app/src/main/java/com/example/physiohut/Appointments.java
@@ -1,2 +1,23 @@
-package com.example.physiohut;public class Appointments {
+package com.example.physiohut;
+
+public class Appointments {
+
+
+    String patientName;
+    String appointmentDate;
+    String appointmentArea;
+    String appointmentTime;
+
+    boolean isVisible = false;
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public Appointments(String patientName, String appointmentDate, String appointmentArea, String appointmentTime) {
+        this.patientName = patientName;
+        this.appointmentDate = appointmentDate;
+        this.appointmentArea = appointmentArea;
+        this.appointmentTime = appointmentTime;
+    }
 }
